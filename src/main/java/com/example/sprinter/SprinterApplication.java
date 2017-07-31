ae8d4205dfa718f255edcb901718c903f9cbe221
castarox <mika.kamil12@gmail.com>
2017-07-31 11:38:54 +0200

    Create project structure

---
+++ src/main/java/com/example/sprinter/SprinterApplication.java
@@ -0,0 +1,12 @@
+package com.example.sprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/sprinter/SprinterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sprinter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprinterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
