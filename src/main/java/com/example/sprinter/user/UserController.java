c34b16841fb438fd7e7356c8f43c8a3e3e65d14e
cj <sigamateusz@gmail.com>
2017-08-19 22:35:22 +0200

    Clean code

--- src/main/java/com/example/sprinter/user/UserController.java
+++ src/main/java/com/example/sprinter/user/UserController.java
@@ -2,18 +2,12 @@ package com.example.sprinter.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import javax.validation.Valid;
-import java.util.List;
 import java.util.Objects;
 
 @Controller
