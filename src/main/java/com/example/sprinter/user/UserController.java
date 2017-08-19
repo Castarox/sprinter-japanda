88c0aa3cb49ae8711eb2474f46500e2ac1bda78a
cj <sigamateusz@gmail.com>
2017-08-19 22:33:42 +0200

    Create session with user attribute

--- src/main/java/com/example/sprinter/user/UserController.java
+++ src/main/java/com/example/sprinter/user/UserController.java
@@ -3,26 +3,31 @@ package com.example.sprinter.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
+@SessionAttributes("user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/login")
-    String logInToTheSitePage(Model model){
-        List<User> userList = userService.getAll();
-        model.addAttribute("Users", userList);
-        return "login";
+    String logInToTheSiteViewPage(ModelMap model){
+        if (model.get("user") == null) {
+            return "login";
+        }
+        return "redirect:/index";
     }
 }
