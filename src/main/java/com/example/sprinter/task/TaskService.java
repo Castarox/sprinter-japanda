21c7727a999eaf0c692652b0fc0e0ba6a2b06ebc
Anna Matras <ania.matras@gmail.com>
2017-09-05 13:44:38 +0000

    Add method to add, delete and update task

--- src/main/java/com/example/sprinter/task/TaskService.java
+++ src/main/java/com/example/sprinter/task/TaskService.java
@@ -1,7 +1,29 @@
 package com.example.sprinter.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
+
+    public void add(Task task) {
+        taskRepository.save(task);
+    }
+
+    public void update(Task task) {
+        taskRepository.save(task);
+    }
+
+    public void remove(Long id) {
+        taskRepository.delete(id);
+    }
 }
