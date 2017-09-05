8e5de0b5bb41fa7893f79e0c9b15860c1c3e2eed
Anna Matras <ania.matras@gmail.com>
2017-09-05 13:10:58 +0000

    Refactor entity name

--- src/main/java/com/example/sprinter/item/ItemService.java
+++
@@ -1,5 +0,0 @@
-package com.example.sprinter.item;
-
-
-public class ItemService {
-}
---
+++ src/main/java/com/example/sprinter/task/TaskController.java
@@ -0,0 +1,7 @@
+package com.example.sprinter.task;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TaskController {
+}
