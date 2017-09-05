f2ea7e18228a68c5b637ad8db3cc3eab1c6d5d46
Anna Matras <ania.matras@gmail.com>
2017-09-05 13:53:55 +0000

    Add session attribute

--- src/main/java/com/example/sprinter/task/TaskController.java
+++ src/main/java/com/example/sprinter/task/TaskController.java
@@ -1,7 +1,10 @@
 package com.example.sprinter.task;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("user")
+
 public class TaskController {
 }
