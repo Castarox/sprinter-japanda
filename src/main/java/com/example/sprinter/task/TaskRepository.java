a360437958ea0829a609f71b15111fe25332531e
Anna Matras <ania.matras@gmail.com>
2017-09-05 13:43:58 +0000

    Remove unused method

--- src/main/java/com/example/sprinter/task/TaskRepository.java
+++ src/main/java/com/example/sprinter/task/TaskRepository.java
@@ -2,10 +2,6 @@ package com.example.sprinter.task;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
-    List<Task> findAllByProjectIdOrder(Long projectId);
-
 }
