d32040338031a6d137ec65280c23e59bdfd04552
castarox <mika.kamil12@gmail.com>
2017-09-05 13:22:16 +0200

    Create getters and setters

--- src/main/java/com/example/sprinter/user_story/UserStory.java
+++ src/main/java/com/example/sprinter/user_story/UserStory.java
@@ -2,15 +2,62 @@ package com.example.sprinter.user_story;
 
 import com.example.sprinter.project.Project;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 
+@Entity
 public class UserStory {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
     private String name;
     private String description;
     @ManyToMany
     @JoinColumn(name = "project_id")
     private Project project;
+
+    UserStory(){}
+
+    UserStory(String name, String description, Project project){
+        this.name = name;
+        this.description = description;
+        this.project = project;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
 }
