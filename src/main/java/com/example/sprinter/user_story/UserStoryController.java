8ab9b1057273436226cf1d0e34f070ca4bdcbc3e
Anna Matras <ania.matras@gmail.com>
2017-09-06 11:18:36 +0000

    Add method to get one user story

--- src/main/java/com/example/sprinter/user_story/UserStoryController.java
+++ src/main/java/com/example/sprinter/user_story/UserStoryController.java
@@ -1,7 +1,34 @@
 package com.example.sprinter.user_story;
 
-/**
- * Created by anna on 06.09.17.
- */
+
+import com.example.sprinter.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@SessionAttributes("user")
+@RequestMapping("/user_story")
 public class UserStoryController {
+    private final UserStoryService userStoryService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    public UserStoryController(UserStoryService userStoryService) {
+        this.userStoryService = userStoryService;
+    }
+
+    @GetMapping("/{id}")
+    String getOne(@PathVariable Long id, Model model) {
+        UserStory userStory = userStoryService.findById(id);
+        model.addAttribute("userStory", userStory);
+        return "project";
+    }
+
+//    @PostMapping("/new")
+//    String add()
+
 }
