f4fb8bad7a754853a32a063cde7398fbb54f5b95
castarox <mika.kamil12@gmail.com>
2017-09-17 11:28:22 +0200

    Create constructor and remove unused imports

--- src/main/java/com/example/sprinter/user_story/UserStoryController.java
+++ src/main/java/com/example/sprinter/user_story/UserStoryController.java
@@ -4,32 +4,27 @@ package com.example.sprinter.user_story;
 import com.example.sprinter.form.UserStoryForm;
 import com.example.sprinter.project.Project;
 import com.example.sprinter.project.ProjectService;
-import com.example.sprinter.task.Task;
-import com.example.sprinter.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.HashSet;
-import java.util.Set;
 
 @Controller
 @SessionAttributes("user")
 @RequestMapping("projects/{project_id}/user_story")
 public class UserStoryController {
 
-    @Autowired
-    private UserService userService;
+    private final ProjectService projectService;
+
+    private final UserStoryService userStoryService;
 
     @Autowired
-    private ProjectService projectService;
-
-    @Autowired
-    private UserStoryService userStoryService;
+    public UserStoryController(ProjectService projectService, UserStoryService userStoryService) {
+        this.projectService = projectService;
+        this.userStoryService = userStoryService;
+    }
 
     @GetMapping("/{id}")
     String getOne(@PathVariable Long id, @PathVariable Long project_id, Model model) {
