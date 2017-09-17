cb88912e9f8e468b6645a1f7da09c0cf30009ae4
Ania Polak <tunczykzcytryna@gmail.com>
2017-09-17 21:14:33 +0200

    Change route for removing

--- src/main/java/com/example/sprinter/project/ProjectRestController.java
+++ src/main/java/com/example/sprinter/project/ProjectRestController.java
@@ -1,22 +1,27 @@
 package com.example.sprinter.project;
 
+import com.example.sprinter.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @SessionAttributes("user")
-@RequestMapping("projects")
+@RequestMapping("projects/remove")
 public class ProjectRestController {
 
     private final ProjectService projectService;
+    private final UserService userService;
 
     @Autowired
-    ProjectRestController(ProjectService projectService) {
+    ProjectRestController(ProjectService projectService, UserService userService) {
         this.projectService = projectService;
+        this.userService = userService;
     }
 
     @PostMapping("/{id}")
-    Boolean removeProject(@PathVariable Long id){
+    Boolean removeProject(@PathVariable Long id, ModelMap model){
         projectService.remove(id);
         return true;
     }
