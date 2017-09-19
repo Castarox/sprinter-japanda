c5d51ec443c02b5d0a960e6ad4da4363ce3cf55e
cj <sigamateusz@gmail.com>
2017-09-19 23:20:31 +0200

    Implement saving NewUser in sendActivateEmail()

--- src/main/java/com/example/sprinter/mail/EmailController.java
+++ src/main/java/com/example/sprinter/mail/EmailController.java
@@ -1,33 +1,48 @@
 package com.example.sprinter.mail;
 
+import com.example.sprinter.new_user.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.*;
 import org.thymeleaf.context.*;
 
+import javax.servlet.http.*;
+import java.util.*;
+
 @Controller
 public class EmailController {
 
     private final EmailSender emailSender;
     private final TemplateEngine templateEngine;
+    private final NewUserService newUserService;
 
     @Autowired
-    public EmailController(EmailSender emailSender,
-                           TemplateEngine templateEngine){
+    public EmailController(EmailSender emailSender, TemplateEngine templateEngine, NewUserService newUserService) {
         this.emailSender = emailSender;
         this.templateEngine = templateEngine;
+        this.newUserService = newUserService;
     }
 
-    @GetMapping("/mailing/{email}")
-    public String sendActivateEmail(@PathVariable String email) {
+    @PostMapping("/mailing")
+    @ResponseBody
+    public Map sendActivateEmail(@RequestBody Map newMap, HttpServletRequest request) {
         Context context = new Context();
         context.setVariable("header", "Registration to Sprinter");
         context.setVariable("title", "Activation link for your account");
-        context.setVariable("description", "Tutaj jakis opis...");
-        String body = templateEngine.process("template", context);
-        email = "sigowww@gmail.com";
+
+        String email = (String) newMap.get("email");
+        String link = newUserService.generateLink(request);
+
+        context.setVariable("link", link);
+        context.setVariable("description", "Activation link");
+        String body = templateEngine.process("activate_email", context);
+
         emailSender.sendEmail(email, "Sprinter - registration", body);
-        return "redirect:/login";
+        NewUser newUser = new NewUser(email,link);
+        newUserService.save(newUser);
+        Map<String, String> map = new HashMap<>();
+        map.put("success", "Activation link sent");
+        return map;
     }
 }
