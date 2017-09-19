50da0dc70b964519154cd1333a262c21982f328c
cj <sigamateusz@gmail.com>
2017-09-19 23:18:59 +0200

    Create new_user package with entity, service & repo

---
+++ src/main/java/com/example/sprinter/new_user/NewUserService.java
@@ -0,0 +1,52 @@
+package com.example.sprinter.new_user;
+
+import com.example.sprinter.user.*;
+import org.h2.util.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import javax.servlet.http.*;
+import java.util.*;
+
+@Service
+public class NewUserService {
+    private final NewUserRepository newUserRepository;
+
+    @Autowired
+    public NewUserService(NewUserRepository newUserRepository) {
+        this.newUserRepository = newUserRepository;
+    }
+
+    public String generateLink(HttpServletRequest request) {
+        String userLink = makeRegistartionPage(request);
+        Random random = new Random();
+        Integer randomNumber;
+        NewUser checkingUser;
+        do {
+            randomNumber = random.nextInt(999999999);
+            checkingUser = newUserRepository.findByLink(userLink + randomNumber.toString());
+        } while (checkingUser != null);
+        userLink += randomNumber.toString();
+        return userLink;
+    }
+
+    public NewUser findByEmail(String email) {
+        return newUserRepository.findByEmail(email);
+    }
+
+    public NewUser save(NewUser newUser) {
+//        NewUser newUser = new NewUser(email, link);
+        return newUserRepository.save(newUser);
+    }
+
+    private String makeUrl(HttpServletRequest request) {
+        return request.getRequestURL().toString();
+    }
+
+    private String makeRegistartionPage(HttpServletRequest request) {
+        String actualUrl = makeUrl(request);
+        String toBeReplaced = "mailing";
+        String pageUrl = actualUrl.replace(toBeReplaced, "");
+        return pageUrl + "registration/";
+    }
+}
