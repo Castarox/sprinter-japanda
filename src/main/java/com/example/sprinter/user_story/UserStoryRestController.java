fd6b9ade15d0ccfb1361e640b06beaaf166ef387
castarox <mika.kamil12@gmail.com>
2017-09-19 12:32:26 +0200

    Add routing for edit

--- src/main/java/com/example/sprinter/user_story/UserStoryRestController.java
+++ src/main/java/com/example/sprinter/user_story/UserStoryRestController.java
@@ -1,8 +1,10 @@
 package com.example.sprinter.user_story;
 
+import com.example.sprinter.form.UserStoryForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -21,6 +23,13 @@ public class UserStoryRestController {
     @PostMapping("/{id}")
     Boolean removeUserStory(@PathVariable Long id){
         userStoryService.remove(id);
+        return true;
+
+    }
+
+    @PostMapping("/edit/{id}")
+    Boolean editUserStory(@PathVariable Long id, @RequestBody UserStoryForm userStoryForm){
+        userStoryService.edit(id, userStoryForm);
         return true;
     }
 
