5e0d7390553a1a294538632e5ddb5fdc77e3e8d6
castarox <mika.kamil12@gmail.com>
2017-09-20 12:39:33 +0200

    Clean code in ErrorController

--- src/main/java/com/example/sprinter/error/ErrorController.java
+++ src/main/java/com/example/sprinter/error/ErrorController.java
@@ -2,7 +2,6 @@ package com.example.sprinter.error;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 public class ErrorController {
