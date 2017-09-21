dbdf1636065145ba39d85a0d533743c8aa33be95
castarox <mika.kamil12@gmail.com>
2017-09-21 08:36:06 +0200

    Refactor project page to materialize

--- src/main/resources/static/js/UserStory.js
+++ src/main/resources/static/js/UserStory.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-
+    $('.modal').modal();
     var currentId;
    $(".delete-button").click(function () {
-       var text = $(this).parent().find(".card-title").html();
-       currentId = $(this).parent().find("h3").data("id");
+       var text = $(this).parents(".card").find("h3").text();
+       currentId = $(this).parents(".m12").attr("id");
        $("#delete").find("p").html(text);
    });
 
@@ -18,7 +18,8 @@ $(document).ready(function () {
             },
             url: url,
             success: function(msg) {
-                $('#'+ currentId).remove()
+                $('#'+ currentId).remove();
+                $(".modal").modal("close");
             }
         })
     })
@@ -30,11 +31,11 @@ $(document).ready(function () {
         $(form).find("input[name='userStoryName']").keyup(function () {
             if ($(this).val().length < 3){
                 $(".storyNameError").text("User story name too short");
-                $(this).css("border", "1px solid red");
+                $(this).css("border-bottom", "1px solid red");
                 isNameCorrect = false;
             } else {
                 $(".storyNameError").text("");
-                $(this).css("border", "1px solid #ccc");
+                $(this).css("border-bottom", "1px solid #ccc");
                 isNameCorrect = true;
             }
         });
@@ -43,11 +44,11 @@ $(document).ready(function () {
             var input =$(this).find("input[name='priority']");
             if (!input.is(':checked')){
                 $(".priorityError").text("Priority is required");
-                input.css("border", "1px solid red");
+                input.css("border-bottom", "1px solid red");
                 isPriorityCorrect = false;
             } else {
                 $(".priorityError").text("");
-                input.css("border", "1px solid #ccc");
+                input.css("border-bottom", "1px solid #ccc");
                 isPriorityCorrect = true;
             }
             if (isNameCorrect && isPriorityCorrect) {
@@ -60,7 +61,7 @@ $(document).ready(function () {
     }
 
     function clear(form) {
-        $(form).find("input[name='userStoryName']").val("").css("border", "1px solid #ccc");
+        $(form).find("input[name='userStoryName']").val("").css("border-bottom", "1px solid #ccc");
         $(form).find(".storyNameError").text("");
 
         $(form).find("input[name='description']").val("");
@@ -83,10 +84,10 @@ $(document).ready(function () {
         var form = $("#edit-user-story-form");
         clear(form);
         validate(form, true, true);
-        currentId = $(this).parents(".col-sm-9").attr("id")
-        var name = $(this).parent().find("h3").text();
-        var description = $(this).parent().find(".description").text();
-        var priority = $(this).parent().find(".priority").text();
+        currentId = $(this).parents(".m12").attr("id");
+        var name = $(this).parents(".card").find("h3").text();
+        var description = $(this).parents(".card").find(".description").text();
+        var priority = $(this).parents(".card").find(".priority").text();
         $(form).find("input[name='userStoryName']").val(name);
         $(form).find("input[name='description']").val(description);
         $(form).find("input[name='priority']").each(function () {
@@ -94,6 +95,8 @@ $(document).ready(function () {
                 $(this).prop('checked', true)
             }
         });
+        $(form).find("label[for='userStoryName']").addClass("active");
+        $(form).find("label[for='description']").addClass("active");
 
     });
 
@@ -103,7 +106,6 @@ $(document).ready(function () {
         var header = $("meta[name='_csrf_header']").attr("content");
         var token = $("meta[name='_csrf']").attr("content");
         var url = "user_story/edit/" + currentId;
-
         var form = $("#edit-user-story-form");
         var name = $(form).find("input[name='userStoryName']").val();
         var description = $(form).find("input[name='description']").val();
@@ -136,7 +138,8 @@ $(document).ready(function () {
                 $(container).find("h3").text(name);
                 $(container).find(".priority").text(priority);
                 $(container).find(".description").text(description);
-                $(".modal-footer").find(".exit").trigger("click")
+                // $(".modal-footer").find(".exit").trigger("click")
+                $('.modal').modal('close')
             }
         })
     })
