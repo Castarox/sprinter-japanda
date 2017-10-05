deb4360646e06e3539d955889f3a0f65c97aa53f
castarox <mika.kamil12@gmail.com>
2017-10-05 10:08:55 +0200

    Change script to delete task

--- src/main/resources/static/js/DeleteTask.js
+++ src/main/resources/static/js/DeleteTask.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
 
     var currentId;
+    var usId;
+
+    $('.delete-task').click(function () {
+        var text = $(this).parents('li').find(".title").text();
+        currentId = $(this).parents('li').attr('id');
+        usId = $('#title_user_story').data('id');
+        $("#delete").find("p").html(text);
+    });
 
     $(".yes").click(function () {
-        currentId = $(this).attr('id');
-        var url = "";
+        console.log(currentId);
+        var url = "tasks/remove/" + currentId;
         var token = $("meta[name='_csrf']").attr("content");
         var header = $("meta[name='_csrf_header']").attr("content");
         $.ajax({
             type: "POST",
-            beforeSend: function(request) {
+            beforeSend: function (request) {
                 request.setRequestHeader(header, token);
             },
             url: url,
-            success: function(msg) {
-                $('#'+ currentId).remove();
+            success: function (msg) {
+                $('#' + currentId).remove();
                 $('.modal').modal('close');
-                window.location.replace('/');
+                // window.location.replace('/');
             }
         })
     })
