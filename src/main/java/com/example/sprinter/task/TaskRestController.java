87a4ddddf02befcf085d50eddba0f87a3b94d90d
castarox <mika.kamil12@gmail.com>
2017-10-05 11:05:12 +0200

    Add path to change state

--- src/main/java/com/example/sprinter/task/TaskRestController.java
+++ src/main/java/com/example/sprinter/task/TaskRestController.java
@@ -1,8 +1,11 @@
 package com.example.sprinter.task;
 
 import com.example.sprinter.form.TaskForm;
+import com.example.sprinter.form.TaskStateForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("projects/{id}/user_story/tasks")
@@ -24,6 +27,18 @@ public class TaskRestController {
     @PostMapping("/edit/{id}")
     Boolean editTask(@PathVariable Long id, @RequestBody TaskForm taskForm) {
         taskService.edit(id, taskForm);
+        return true;
+    }
+
+    @PostMapping("/edit/{id}/state")
+    Boolean editState(@PathVariable Long id, @RequestBody TaskStateForm taskStateForm){
+        taskService.changeState(id, taskStateForm);
+        System.out.println(taskStateForm.getState());
+        return true;
+    }
+
+    @GetMapping("/edit/{id}/state")
+    Boolean haha (){
         return true;
     }
 }
