0f91ee0ffa5c55587f0b1754fa6644c3e7948a5e
castarox <mika.kamil12@gmail.com>
2017-10-05 10:09:47 +0200

    Add script to edit user story

--- src/main/resources/static/js/EditUserStory.js
+++ src/main/resources/static/js/EditUserStory.js
@@ -1,39 +1,33 @@
-// $(document).ready(function () {
-//     function update () {
-//         console.log($(document).find('.priority').find('img').data('id'))
-//         // var header = $("meta[name='_csrf_header']").attr("content");
-//         // var token = $("meta[name='_csrf']").attr("content");
-//         // var url = "user_story/edit/" + currentId;
-//         // var form = $("#edit-user-story-form");
-//         // var name = $(form).find("input[name='userStoryName']").val();
-//         // var description = $(form).find("input[name='description']").val();
-//         // var projectId = $(form).find("input[name='projectId']").val();
-//         // var priority;
-//         //
-//         // var data = {userStoryName:name,
-//         //     description: description,
-//         //     priority: priority,
-//         //     projectId: projectId};
-//         //
-//         // $.ajax({
-//         //     type: "POST",
-//         //     beforeSend: function (request) {
-//         //         request.setRequestHeader(header, token);
-//         //     },
-//         //     url: url,
-//         //     data: JSON.stringify(data),
-//         //     contentType : "application/json",
-//         //     dataType : "json",
-//         //     success: function (response) {
-//         //         var container = $("#"+ currentId);
-//         //         $(container).find("h3").text(name);
-//         //         $(container).find(".priority").text(priority);
-//         //         $(container).find(".description").text(description);
-//         //         // $(".modal-footer").find(".exit").trigger("click")
-//         //         $('.modal').modal('close')
-//         //     }
-//         // })
-//     }
-//
-//     $('#title_user_story').change(update())
-// });
+$(document).ready(function () {
+
+
+    $('.save').click(function (e) {
+        e.preventDefault();
+        var priority = $(document).find('.priority').find('img').data('id');
+        var header = $("meta[name='_csrf_header']").attr("content");
+        var token = $("meta[name='_csrf']").attr("content");
+        var currentId = $('#title_user_story').data('id');
+        var url = "/projects/user_story/edit/" + currentId;
+        var name = $('#title_user_story').val();
+        var description = $('#description_user_story').text();
+        var projectId = $('#delete-user-story').find('.yes-story').data('project');
+        var data = {userStoryName:name,
+            description: description,
+            priority: priority,
+            projectId: projectId};
+        console.log(data);
+
+        $.ajax({
+            type: "POST",
+            beforeSend: function (request) {
+                request.setRequestHeader(header, token);
+            },
+            url: url,
+            data: JSON.stringify(data),
+            contentType : "application/json",
+            dataType : "json",
+            success: function (response) {
+            }
+        })
+    })
+});
